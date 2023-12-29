dd8b68d9d0267ecaa27ed55001c223ba3c52f019
김아영 <72481734+koyoung99@users.noreply.github.com>
2023-12-29 16:59:01 +0900

    배열_20231229

---
+++ src/com/javaex/ex04/Ex01.java
@@ -0,0 +1,15 @@
+package com.javaex.ex04;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i < 6; i++) {
+			int num = (int) (Math.random() * 45) + 1;
+			System.out.print(num + " ");
+
+		}
+
+	}
+
+}
---
+++ src/com/javaex/ex04/Ex02.java
@@ -0,0 +1,18 @@
+package com.javaex.ex04;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+
+		double[] dArray = new double[3];
+		dArray[0] = 0.3;
+		dArray[1] = 7.2;
+		dArray[2] = 4;
+
+		for (int i = 0; i < dArray.length; i++) {
+			System.out.println(dArray[i]);
+		}
+
+	}
+
+}
